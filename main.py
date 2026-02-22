e17bd36514a108b1c2e2662c16e8cf1f6e2e14e4
jw0808-blip <jw0808@gmail.com>
2026-02-22 15:54:53 -0600

    Final main.py - All platforms integrated with loaded keys

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ import discord
 from discord.ext import commands
 import os
 import requests
+import time
+import base64
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+KALSHI_KEY_ID = os.getenv('KALSHI_API_KEY_ID')
+KALSHI_PEM = os.getenv('KALSHI_PRIVATE_KEY_PEM')
+ROBINHOOD_PUBLIC_KEY = os.getenv('ROBINHOOD_PUBLIC_KEY')
+ROBINHOOD_API_KEY = os.getenv('ROBINHOOD_API_KEY')
+ROBINHOOD_PRIVATE_KEY = os.getenv('ROBINHOOD_PRIVATE_KEY')
+POLYMARKET_API_KEY = os.getenv('POLYMARKET_API_KEY')
+POLYMARKET_SECRET = os.getenv('POLYMARKET_SECRET')
+POLYMARKET_PASSPHRASE = os.getenv('POLYMARKET_PASSPHRASE')
 
 @bot.event
 async def on_ready():
